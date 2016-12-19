e43bd6299dd822a5ce6aedfebf58025a694631c2
Robert Doggett <robert@peacedudes.com>
2016-12-18 19:06:38 -0800

    Add empty project

---
+++ QuoteHere/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QuoteHere
+//
+//  Created by robert on 12/18/16.
+//  Copyright © 2016 Robert Doggett. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
